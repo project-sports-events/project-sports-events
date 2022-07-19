9c338e63c49aa40e770165f69886cf07d773914c
zubairmalek <zubairmalek95@gmail.com>
2022-07-19 12:40:28 +0100

    added create even form and show events

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,12 +1,46 @@
 const router = require("express").Router();
-
+const Event = require("../models/Event.model");
 /* GET home page */
+
 router.get("/", (req, res, next) => {
-  res.render("events");
+  Event.find().then((data) => {
+    res.render("events", data);
+  });
 });
 
 router.get("/create-event", (req, res, next) => {
   res.render("create-event");
+});
+
+router.post("/create-event", (req, res, next) => {
+  console.log(req.body);
+  const { typeOfSport, title, location, numberOfRequiredPlayers, price, date } =
+    req.body;
+  const author = req.session.user._id;
+
+  Event.create({
+    typeOfSport,
+    title,
+    location,
+    numberOfRequiredPlayers,
+    price,
+    author,
+    date,
+  })
+    .then(() => {
+      Event.find().then((data) => {
+        res.redirect("/events"), data;
+      });
+    })
+    // .then(
+    //   (data) => console.log("Saved into the database! ", data),
+    //   res.redirect("/events")
+    // )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log(typeOfSport);
 });
 
 module.exports = router;
